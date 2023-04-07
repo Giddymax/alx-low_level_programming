675700be12847e5c5c8bc954f57c31a808352171
Giddymax <gkpodonu@gmail.com>
2023-04-07 06:43:52 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,17 +4,9 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-/**
- * create_file - creates a file.
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- *
- * Return: -1 if failure else 1
- */
-
 int create_file(const char *filename, char *text_content)
 {
-	int m, w;
+	int o, w;
 	int len = 0;
 
 	if (filename == NULL)
@@ -26,13 +18,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	m = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
-	if (m == -1 || w == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(m);
+	close(o);
 
 	return (1);
 }
